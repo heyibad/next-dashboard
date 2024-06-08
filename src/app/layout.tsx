2dc7d069741825b763313538d968aa28f00c7b94
Muhammad Ibad <ibad3572@gmail.com>
2024-06-08 20:31:31 +0500

    refactor: Update layout component with new font and components
    This commit updates the layout component in the src/app/layout.tsx file. It replaces the imported font from "next/font/google" with the "Ubuntu" font. Additionally, it adds the Navbar and Sidebar components from "@/components" to the layout. The body element now uses the "ubuntu" font class instead of "inter". The Navbar component is customized with a white text color and a primary background color. The Sidebar component is hidden on small screens and displayed on medium screens. The children of the layout are wrapped in a div with a maximum width of 1140px.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,43 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
-const inter = Inter({ subsets: ["latin"] });
+const ubuntu = Ubuntu({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: "Nextpress Dashboard",
-  description: "Dashboard which was built on the top of Nextjs | Tailwind | TypeScript | React Hook Form | ShadCn | ZOD",
+    title: "Nextpress Dashboard",
+    description:
+        "Dashboard which was built on the top of Nextjs | Tailwind | TypeScript | React Hook Form | ShadCn | ZOD",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={ubuntu.className}>
+                <Navbar
+                    className="text-white bg-primary dark:bg-slate-700"
+                    LogoName="DASHBOARD"
+                    name="Muhmmad Ibad"
+                    imageUrl="https://github.com/heyibad.png"
+                />
+                <div className="flex">
+                    <div className="h-[100vh] hidden md:block w-[300px]">
+                        <Sidebar />
+                    </div>
+                    <div
+                    className="w-full md:max-w-[1140px] p-5 "
+                    >{children}</div>
+                </div>
+            </body>
+        </html>
+    );
 }
